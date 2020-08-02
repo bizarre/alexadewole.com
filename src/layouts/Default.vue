866fdba3f551585c5dc288ab1399bd99e26c21b0
Alex <me@a9e.sh>
2020-08-01 18:56:36 -0700

    Add fade page transition

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,7 +2,11 @@
   <div class="w-full flex">
     <div class="p-4 w-full md:w-2/3 md:m-auto md:px-0 md:pt-40">
       <site-header/>
-      <slot/>
+      <transition name="fade" appear>
+        <main>
+          <slot/>
+        </main>
+      </transition>
     </div>
   </div>
 </template>
@@ -26,5 +30,13 @@ body {
   body {
     background: linear-gradient(155.36deg, #F2F1FF 28.15%, #EDFCFF 62.08%, #FEFFE2 91.4%)
   }
+}
+
+.fade-enter-active {
+  transition: opacity .2s;
+}
+
+.fade-enter {
+  opacity: 0;
 }
 </style>
